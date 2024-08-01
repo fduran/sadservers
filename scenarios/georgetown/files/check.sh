d0bc3f3a464d1aa1bcc8fe3b72e47a141804e678
jodybro <ajodasattaur@gmail.com>
2024-08-01 19:42:35 -0400

    files for georgetown scenario

--- scenarios/georgetown/files/check.sh
+++ scenarios/georgetown/files/check.sh
@@ -4,10 +4,10 @@ BUILD_MOUNT_POINT="/tmp/ephemeral-build"
 
 # Check if the build mount point exists
 function check_build_mount_point {
-    if [ ! -d $BUILD_MOUNT_POINT ]; then
-        echo "Build mount point $BUILD_MOUNT_POINT does not exist"
-        exit 1
-    fi
+  if [ ! -d $BUILD_MOUNT_POINT ]; then
+    echo -b "NO"
+    exit 1
+  fi
 }
 
 # Function to check if both example-build-artifact.txt and
@@ -15,23 +15,20 @@ function check_build_mount_point {
 # BOTH files need to exist concurrently
 function check_build_artifacts {
   BUILD_FILES=(
-      "example-build-artifact.txt"
-      "secondary-artifact.txt"
-    )
+    "example-build-artifact.txt"
+    "secondary-artifact.txt"
+  )
   for file in "${BUILD_FILES[@]}"; do
     if [ ! -f "$BUILD_MOUNT_POINT/$file" ]; then
-        echo "Build artifact $file does not exist"
-        exit 1
+      echo -b "NO"
+      exit 1
+    else
+      echo -b "OK"
+      exit 0
     fi
   done
 }
 
-# Check if the build mount point exists
+# Check if one or both functions executed successfull
 check_build_mount_point
-
-# Infinitely loop to check if the build artifacts exist
-while true; do
-    check_build_artifacts
-    sleep 5
-done
-
+check_build_artifacts
